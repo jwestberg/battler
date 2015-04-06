30d2cb43b6bb3c6f81e45cc31fe1e4e002f1f3fe
Joel Westberg <joel.westberg@findwise.com>
2015-04-06 11:29:50 +0000

    Finishes deployment logic

--- battle.js
+++ battle.js
@@ -10,6 +10,8 @@ function Battle(attacker, defender, terrain, crossing) {
   
   attacker.deploy(terrain);
   defender.deploy(terrain);
+  
+  
 }
 
 function General(fire, shock, manouver, siege) {
@@ -59,34 +61,66 @@ function Army(general, country, num_infantry, num_cavalry, num_artillery) {
   this.firstrow = []
   this.secondrow = []
   
-  this.deploy = function(terrain) {
-    var width = Math.floor(tech.getModifiers(this.country.tech_level).combat_width * terrain.combat_width); //TODO: Verify floor is correct
-    var firstrowreserved = 2*(Math.floor(width/10)) + 2; //TODO: Observe in the wild
-    if (width-firstrowreserved < this.infantry.length) {
-      for (var i = 0; i<width-firstrowreserved; i++) {
-        this.firstrow[i+firstrowreserved/2] = this.infantry[i];
-        this.infantry[i].deployed = true
+  this._deployCavalry = function(width, cavalryreserved, row) {
+    cav = _und.filter(this.cavalry, function(cav) { return !cav.deployed; })
+    
+    for(i = 0; i<cavalryreserved && i<cav.length; i++) {
+      if(i%2 === 0) {
+        row[i/2] = cav[i];
+      } else {
+        row[width-Math.floor(i/2)] = cav[i];
       }
-      for(var i = 0; i<firstrowreserved && i<this.cavalry.length; i++) {
-        if(i%2 == 0) {
-          this.firstrow[i/2] = this.cavalry[i];
-        } else {
-          this.firstrow[width-Math.ceil(i/2)] = this.cavalry[i];
-        }
-        this.cavalry[i].deployed = true;
-      }
-      for(var i=0; i<width; i++) {
-        if(typeof this.firstrow[i] == 'undefined') {
-          this.firstrow[i] = _und.find(this.infantry, function(unit) { return !unit.deployed; });
-          this.firstrow[i].deployed = true;
+      cav[i].deployed = true;
+    }
+  }
+  
+  this._fillInfantry = function(width, row) {
+    for(i=0; i<width; i++) {
+      x = (i%2 === 0) ? Math.floor(width/2)-Math.floor(i/2) : Math.floor(width/2)+Math.ceil(i/2);
+      if(typeof row[x] == 'undefined') {
+        row[x] = _und.find(this.infantry, function(unit) { return !unit.deployed; });
+        if(typeof row[x] != 'undefined') {
+          row[x].deployed = true; 
         }
       }
     }
+  }
+  
+  this.deploy = function(terrain) {
+    var width = Math.floor(tech.getModifiers(this.country.tech_level).combat_width * terrain.combat_width); //TODO: Verify floor is correct
+    var cavalryreserved = 2*(Math.floor(width/10)) + 2; //TODO: Observe in the wild
+    for (i = 0; i<width-cavalryreserved && i<this.infantry.length; i++) {
+      if(i%2 === 0) {
+        this.firstrow[Math.floor(width/2)-i/2] = this.infantry[i];
+      } else {
+        this.firstrow[Math.floor(width/2)+Math.ceil(i/2)] = this.infantry[i]
+      }
+      this.infantry[i].deployed = true
+    }
+    this._deployCavalry(width, cavalryreserved, this.firstrow)
+    this._fillInfantry(width, this.firstrow)
+    for (i = 0; i<width && i<this.artillery.length; i++) {
+      var location = (i%2 === 0) ? Math.floor(width/2)-i/2 : Math.floor(width/2)+Math.ceil(i/2);
+      if(typeof this.firstrow[location] != 'undefined' && this.firstrow[location].type == 'infantry') {
+        this.secondrow[location] = this.artillery[i];
+        this.secondrow[location].deployed = true;
+      } else if (typeof this.firstrow[location] == 'undefined') {
+        this.firstrow[location] = this.artillery[i];
+        this.firstrow[location].deployed = true;
+      }
+    }
+    this._deployCavalry(width, cavalryreserved, this.secondrow)
+    this._fillInfantry(width, this.secondrow)
   };
   
   this.asciiRows = function() {
-    return _und.map(this.firstrow, function(unit) { return unit.type.charAt(0); }).join('')+
-      '\n'+_und.map(this.secondrow, function(unit) { return unit.type.charAt(0); }).join('')
+    fr = ''
+    sr = ''
+    for(i = 0; i<this.firstrow.length; i++) {
+      fr += typeof this.firstrow[i] != 'undefined' ? this.firstrow[i].type.charAt(0) : ' ';
+      sr += typeof this.secondrow[i] != 'undefined' ? this.secondrow[i].type.charAt(0) : ' ';
+    }
+    return fr+'\n'+sr;
   }
 }
 
@@ -103,10 +137,8 @@ var attacker_country = new Country(10, "western", 2.5, 2.5)
 var defender_country = new Country(10, "chinese", 2.5, 2.5)
 var general = new General(1,1,1,1);
 
-var attacker_army = new Army(general, attacker_country, 1000, 2, 1);
+var attacker_army = new Army(general, attacker_country, 7, 8, 100);
 var defender_army = new Army(general, defender_country, 5, 1, 1);
 var battle = new Battle(attacker_army, defender_army, terrains.mountains, -1)
 
 console.log(attacker_army.asciiRows())
-
-console.log(attacker_army.asciiRows().length)
